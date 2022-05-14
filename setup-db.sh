dbbeb80f303c654d143d5c734bced325898ff424
Tomás Dias <tomasalexdias@gmail.com>
2022-05-14 06:50:33 +0100

    :wrench: Change database sample

--- setup-db.sh
+++ setup-db.sh
@@ -1,1 +1,1 @@
-mongoimport --uri "mongodb://127.0.0.1:27017/" --db "battery-cycles" --collection="offline" --file "sample.json"
+mongoimport --uri "mongodb://127.0.0.1:27017/" --db "battery-cycles" --collection "offline" --file "sample.json"
