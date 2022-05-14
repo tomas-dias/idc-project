dbd79002eb5fa033f7260a1e13f68e53cfc6b82a
Tomás Dias <tomasalexdias@gmail.com>
2022-05-14 07:50:37 +0100

    :wrench: Change database script

--- setup-db.sh
+++ setup-db.sh
@@ -1,1 +1,1 @@
-mongoimport --uri "mongodb://127.0.0.1:27017/" --db "battery-cycles" --collection "offline" --file "sample.json" --jsonArray
+mongoimport --uri "mongodb://127.0.0.1:27017/" --db "battery-cycles" --collection "offline" --file "sample2.json" --jsonArray
