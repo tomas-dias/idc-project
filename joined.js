2de24b6bb31292ad440e247e81b9a87aee3497e3
Marques52769 <80492634+Marques52769@users.noreply.github.com>
2022-05-18 01:45:12 +0100

    Add files via upload

---
+++ joined.js
@@ -0,0 +1,33 @@
+var query = [
+    {
+        $facet: {
+            query1: [
+                { "$match" : {"battery_ID" : msg.payload}},
+                { "$group": {
+                "_id": {
+                    "type" : "$type"
+                },
+                "counter" : { "$sum" : 1},
+                "avgTime": {"$avg": "$elapsed_time"},
+                "standardDeviation": {"$stdDevSamp": "$elapsed_time"}
+                }}
+            ],
+            query2: [ 
+                {"$unwind" : "$temp_battery"},
+                { "$match" : {"battery_ID" : msg.payload}},
+                { "$group": {
+                    "_id": {
+                        "type" : "$type"
+                    },
+                    "minTempBattery" : {"$min" : "$temp_battery"}
+                }}
+             ]
+        }
+    }
+]
+
+var newMsg = {
+    payload: query 
+}
+
+return newMsg
